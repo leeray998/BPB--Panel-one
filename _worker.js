32de776a119ba93fabb545f44aae18cbf11c43a1
ericaltman <93544484+leeray998@users.noreply.github.com>
2024-08-24 23:45:50 +0800

    Update _worker.js
    46705513-2dd0-460f-8782-8eab38b6119f

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '46705513-2dd0-460f-8782-8eab38b6119f';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
